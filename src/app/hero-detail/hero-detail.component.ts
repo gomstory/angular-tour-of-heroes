2f83222a514e3ec88c740d0b6fe3c52b70346b8a
Arkom Madaehoh <arkom.madaehoh@ibm.com>
2021-02-10 19:27:17 +0700

    Fix nullable value

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -8,10 +8,10 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: [ './hero-detail.component.css' ]
+  styleUrls: [ './hero-detail.component.scss' ]
 })
 export class HeroDetailComponent implements OnInit {
-  hero: Hero;
+  hero: Hero = { id: 11, name: "" };
 
   constructor(
     private route: ActivatedRoute,
